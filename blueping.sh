ed5c09dc1e18cf931faa7e1947740592e6bab0ae
WOOHYUNG CHOI <whchoi98@gmail.com>
2022-09-19 21:15:23 +0900

    add init

---
+++ blueping.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+date
+cat ./blueserver.txt |  while read output
+do
+    ping -c 3 "$output" > /dev/null
+    if [ $? -eq 0 ]; then
+    echo "SERVER $output is up" 
+    else
+    echo "SERVER $output is down"
+    fi
+done
